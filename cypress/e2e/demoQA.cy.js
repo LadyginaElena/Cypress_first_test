494b832d7611b939aab8dcaaa868b7810a60bd51
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-05-09 20:44:29 +0700

    added demoQA tests

--- cypress/e2e/demoQA.cy.js
+++ cypress/e2e/demoQA.cy.js
@@ -1,12 +1,50 @@
 ///<reference types="cypress"/>
 
-describe("element",()=>{
-    before(()=>{
-        cy.visit("https://demoqa.com/")
+describe("Demo QA tests", () => {
+    beforeEach(() => {
+        cy.visit("https://demoqa.com");
     })
-    it("testElements",()=>{
-        cy.get("div.category-cards .card-body h5").contains("Elements").click()
-        cy.get("#item-0").contains("Text Box").click()
-        cy.get("#userName").type("Elena")
+    const expectedMenuItemNames = ["Elements", "Forms", "Alerts, Frame & Windows", "Widgets", "Interactions", "Book Store Application",]
+    it("Verification menu item names", () => {
+        cy.get('.card').should("have.length", expectedMenuItemNames.length).each(($el, idx) => {
+            expect($el.text()).to.be.equal(expectedMenuItemNames[idx]);
+        });
+
     })
+    it("Verification menu item names Variant 2", () => {
+        cy.get('.card')
+            .should("have.length", expectedMenuItemNames.length)
+            .then(($els) => {
+                return Cypress.$.makeArray($els).map($el => $el.innerText)
+            })
+            .should("deep.equal", expectedMenuItemNames)
+    });
+
+    it("Verification menu item names Variant 3", () => {
+        cy.get('.card')
+            .should("have.length", expectedMenuItemNames.length)
+            .then(($els) => {
+                let actual = Cypress.$.makeArray($els).map($el => $el.innerText)
+                expect(actual).to.be.deep.equal(expectedMenuItemNames)
+            })
+    });
+
+    it("Verification menu item names Variant 4", () => {
+        cy.get('.card')
+            .should("have.length", expectedMenuItemNames.length)
+            .then(($els) => {
+                return Cypress._.map($els, "innerText")
+            })
+            .should("deep.equal", expectedMenuItemNames)
+    });
+    it.only("Form filling", () => {
+        cy.get('.card').contains("Forms").click()
+        cy.get(".element-group:nth-child(2)>div").click()
+        cy.get("#firstName")
+            .should("have.text", "")
+            .type("Stan")
+            .should("have.value", "Stan")
+        cy.get("#submit").click()
+        cy.get("#firstName").should("have.css", "border-color", "rgb(40, 167, 69)")
+    });
 })
