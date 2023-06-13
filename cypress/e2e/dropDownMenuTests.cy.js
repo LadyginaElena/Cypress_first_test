b0f478369a394b48ee59b13dde55b8ce105df9d5
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-06-13 05:59:18 +0000

    refactor test

--- cypress/e2e/dropDownMenuTests.cy.js
+++ cypress/e2e/dropDownMenuTests.cy.js
@@ -11,12 +11,15 @@ describe("Dropdown menu tests", () => {
         су.visit("https://primereact.org/dropdown/")
         cy.get(".doc-main .py-3:nth-of-type(8) .p-dropdown").click()
 
-        function searchOption() {
+        function searchOption(level=0) {
+            if (level>25){
+                throw new Error("Exceeded max recursion level")
+            }
             cy.get(".doc-main .py-3:nth-of-type(8) .p-dropdown .p-dropdown-label").then(($el => {
                 const activeItem = $el.text();
                 if (activeItem != "Item #25") {
                     cy.wrap($el).type("{downarrow}")
-                    searchOption()
+                    searchOption(++level)
                 }
                 cy.wrap($el).click()
             }))
