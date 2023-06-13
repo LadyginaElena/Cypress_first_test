3e93d559eed544b8bff0a20e6c7978ace217e3d1
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-06-13 05:50:44 +0000

    Virtual Scroll dropdown menu test added

--- cypress/e2e/dropDownMenuTests.cy.js
+++ cypress/e2e/dropDownMenuTests.cy.js
@@ -2,12 +2,27 @@
 
 describe("Dropdown menu tests", () => {
 
-    it("React dropdown menu", () => {
-        cy.visit("")
+    it("Select State in Practice Form Demo QA", () => {
+        cy.visit("https://demoqa.com/automation-practice-form")
         cy.get("")
     });
 
-    it("Virtual Scroll", () => {
+    it("Virtual Scroll dropdown menu", () => {
+        су.visit("https://primereact.org/dropdown/")
+        cy.get(".doc-main .py-3:nth-of-type(8) .p-dropdown").click()
 
+        function searchOption() {
+            cy.get(".doc-main .py-3:nth-of-type(8) .p-dropdown .p-dropdown-label").then(($el => {
+                const activeItem = $el.text();
+                if (activeItem != "Item #25") {
+                    cy.wrap($el).type("{downarrow}")
+                    searchOption()
+                }
+                cy.wrap($el).click()
+            }))
+        }
+        searchOption();
+        cy.get(".doc-main .py-3:nth-of-type(8) .p-dropdown .p-dropdown-label")
+        .should("have.text", "Item #25")
     });
 })
