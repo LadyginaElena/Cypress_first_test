19c535ad13db98aa3b8e62d0a179c0c818c188b5
Elena Ladygina <Ladygina_ev@yahoo.com>
2023-06-13 04:52:39 +0000

    added dropDownMenuTests file

---
+++ cypress/e2e/dropDownMenuTests.cy.js
@@ -0,0 +1,13 @@
+///<reference types="cypress"/>
+
+describe("Dropdown menu tests", () => {
+
+    it("React dropdown menu", () => {
+        cy.visit("")
+        cy.get("")
+    });
+
+    it("Virtual Scroll", () => {
+
+    });
+})
